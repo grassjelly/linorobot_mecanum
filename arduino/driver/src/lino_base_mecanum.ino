7de33f65dd792197fca42322ec532530c12e58f3
Juan Miguel Jimeno <jimenojmm@gmail.com>
2016-12-02 15:58:54 +0800

    cleaning up

--- arduino/driver/src/lino_base_mecanum.ino
+++ arduino/driver/src/lino_base_mecanum.ino
@@ -198,7 +198,7 @@ void loop()
       motor1.required_rpm = -y_rpm;
       motor3.required_rpm =  y_rpm;
       //right side
-      motor2.required_rpm =  motor3.required_rpm;
+      motor2.required_rpm = motor3.required_rpm;
       motor4.required_rpm = motor1.required_rpm;
     }
     else
