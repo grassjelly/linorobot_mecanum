1b3a458d952c4ebb057ab08b79fe4f85746dcbb9
jimenojmm@gmail.com <jimenojmm@gmail.com>
2017-01-22 23:12:33 +0800

    updated pinouts for new teensy shield

--- arduino/firmware/lib/config/lino_base_config.h
+++ arduino/firmware/lib/config/lino_base_config.h
@@ -6,50 +6,56 @@
 
 #define DEBUG 0
 
+/*BTS7960 PID
 #define K_P 0.05 // P constant
-#define K_I 0.9 // I constant
+#define K_I 0.5 // I constant
 #define K_D 0.1 // D constant
+*/
+
+#define K_P 0.6 // P constant
+#define K_I 0.3 // I constant
+#define K_D 0.5 // D constant
 
 // define your robot' specs here
-#define MAX_RPM 45 // motor's maximum RPM
-#define COUNTS_PER_REV 4000 // wheel encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.15 // wheel's diameter in meters
+#define MAX_RPM 330 // motor's maximum RPM
+#define COUNTS_PER_REV 1650 // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.10 // wheel's diameter in meters
 #define PWM_BITS 8 // PWM Resolution of the microcontroller
-#define BASE_WIDTH 0.43 // width of the plate you are using
+#define BASE_WIDTH 0.23 // width of the plate you are using
 
 // ENCODER PINS
 // left side encoders pins
-#define MOTOR1_ENCODER_A 17 // front_A
-#define MOTOR1_ENCODER_B 16 // front_B
+#define MOTOR1_ENCODER_A 15 // front_A
+#define MOTOR1_ENCODER_B 14 // front_B
 
-#define MOTOR3_ENCODER_A 15 // rear_A
-#define MOTOR3_ENCODER_B 14 // rear_B
+#define MOTOR3_ENCODER_A 17 // rear_A
+#define MOTOR3_ENCODER_B 16 // rear_B
 
 // right side encoders pins
 #define MOTOR2_ENCODER_A 12 // front_A
 #define MOTOR2_ENCODER_B 11 // front_B
 
-#define MOTOR4_ENCODER_A 9  // rear_A
-#define MOTOR4_ENCODER_B 10 // rear_B
+#define MOTOR4_ENCODER_A 10  // rear_A
+#define MOTOR4_ENCODER_B 9 // rear_B
 
 #ifdef L298_DRIVER
   //left side motor pins
-  #define MOTOR1_PWM 23
-  #define MOTOR1_IN_A 17
-  #define MOTOR1_IN_B 16
+  #define MOTOR1_PWM 21
+  #define MOTOR1_IN_A 20
+  #define MOTOR1_IN_B 1
 
   #define MOTOR3_PWM 22
-  #define MOTOR3_IN_A 14
-  #define MOTOR3_IN_B 15
+  #define MOTOR3_IN_A 23
+  #define MOTOR3_IN_B 0
 
   //right side motor pins
-  #define MOTOR2_PWM 21
-  #define MOTOR2_IN_A 13
-  #define MOTOR2_IN_B 12
+  #define MOTOR2_PWM 5
+  #define MOTOR2_IN_A 8
+  #define MOTOR2_IN_B 6
 
-  #define MOTOR4_PWM 20
-  #define MOTOR4_IN_A 10
-  #define MOTOR4_IN_B 11
+  #define MOTOR4_PWM 4
+  #define MOTOR4_IN_A 2
+  #define MOTOR4_IN_B 3
 #endif
 
 #ifdef BTS7960_DRIVER
